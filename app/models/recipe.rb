5acfd40e10321c1ef6471092d87f1d1ffccadc85
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-02-25 17:51:49 -0600

    Add ingredient model and many to many assoc with recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,8 @@ class Recipe < ApplicationRecord
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   belongs_to :chef
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
   validates :name, presence: true
   validates :chef_id, presence: true
   validates :description, presence: true, length: { minimum: 5, maximum: 500 }
